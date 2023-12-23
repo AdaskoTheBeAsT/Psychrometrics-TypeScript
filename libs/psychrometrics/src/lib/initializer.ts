1a186fb1a461c0762ec6151d91cf689b02d61922
Adam Pluciński <adaskothebeast@gmail.com>
2023-12-24 00:35:46 +0100

    esm and cjs publish

--- libs/psychrometrics/src/lib/initializer.ts
+++ libs/psychrometrics/src/lib/initializer.ts
@@ -15,11 +15,7 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import {
-  MonotoneCubicHermitInterpolation,
-  NumberTuple,
-  Spline,
-} from '@adaskothebeast/splines/src/MonotoneCubicHermitInterpolation';
+import { MonotoneCubicHermitInterpolation, NumberTuple, Spline } from '@adaskothebeast/splines';
 
 import { temperatureToSaturationVaporPressure } from './temperature-to-saturation-vapor-pressure';
 
--- publish.bat
+++ publish.bat
@@ -1,4 +1,5 @@
 xcopy .\LICENSE .\dist\libs\psychrometrics\ /Y
 xcopy .\README.md .\dist\libs\psychrometrics\ /Y
+node .\tools\scripts\cleanPackage.mjs
 cd dist/libs/psychrometrics
 REM npm publish --tag=latest --access public
